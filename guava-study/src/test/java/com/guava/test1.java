a7d9bc409f490d3ff199fdcbcc3bb1dbc27bdf72
zhaobohao <zhaobohao@sohu.com>
2019-07-09 12:14:09 +0800

    init

---
+++ guava-study/src/test/java/com/guava/test1.java
@@ -0,0 +1,4 @@
+package com.guava;
+
+public class test1 {
+}
---
+++ guava-study/src/test/java/com/testng/HelloTestNG.java
@@ -0,0 +1,24 @@
+package com.testng;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class HelloTestNG {
+    @BeforeClass
+    public void setUp() {
+        // code that will be invoked when this test is instantiated
+    }
+
+    @Test(groups = { "fast" })
+    public void aFastTest() {
+        System.out.println("Fast test");
+    }
+
+    @Test(groups = { "slow" })
+    public void aSlowTest() {
+        System.out.println("Slow test");
+
+
+
+    }
+}
