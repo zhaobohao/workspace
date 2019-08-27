bc554b9e24cfaaad2eaffce08cbb3ea24be812f7
zhaobohao <zhaobohao@sohu.com>
2019-08-27 16:45:28 +0800

    新增加job test的相关文件

---
+++ springboot/spring-batch-test/src/test/java/com/batch/test/BatchTest.java
@@ -0,0 +1,14 @@
+package com.batch.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {SpringBatchApplication.class},webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)// 指定启动类
+@Slf4j
+public class BatchTest extends AbstractTestNGSpringContextTests {
+}
