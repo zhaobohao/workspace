623877ae7beb613a732def8f98c66134feeca17c
zhaobohao <zhaobohao@sohu.com>
2019-10-15 17:38:21 +0800

    table的测试用例

--- guava-study/src/test/java/com/guava/collection/TableTest.java
+++ guava-study/src/test/java/com/guava/collection/TableTest.java
@@ -1,6 +1,9 @@
 package com.guava.collection;
 
+import cn.hutool.core.collection.CollUtil;
+import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.HashBiMap;
 import com.google.common.collect.Table;
 import lombok.extern.log4j.Log4j2;
 import org.testng.annotations.Test;
@@ -10,14 +13,26 @@ public class TableTest {
     @Test
     public void test1()
     {
-        Table<Integer, Integer, Integer> weightedGraph = HashBasedTable.create();
-        weightedGraph.put(1, 2, 4);
-        weightedGraph.put(1, 3, 20);
-        weightedGraph.put(2, 3, 5);
+        Table<Integer, Integer, String> weightedGraph = HashBasedTable.create();
+        weightedGraph.put(1, 1, "男");
+        weightedGraph.put(2, 1, "女");
+        weightedGraph.put(3, 1, "它");
+
+        weightedGraph.put(1, 2, "女");
+        weightedGraph.put(2, 2, "男");
+        weightedGraph.put(3, 2, "它");
+
+        weightedGraph.put(1, 3, "男");
+        weightedGraph.put(2, 3, "女");
+        weightedGraph.put(3, 3, "它");
+
+
 
         log.info(weightedGraph.row(1)); // returns a Map mapping v2 to 4, v3 to 20
         log.info(weightedGraph.column(3)); // returns a Map mapping v1 to 20, v2 to 5
 
+        BiMap  bm= HashBiMap.create(weightedGraph.column(2));
 
+        log.info(bm.inverse());
     }
 }
