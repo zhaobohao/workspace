b7e09d23cfa9356168b14a1041d332d908744021
zhaobohao <zhaobohao@sohu.com>
2019-11-05 11:22:30 +0800

    init vue ui

---
+++ SpringCloudDev/springcloud-vue-ui/src/views/dashboard/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="dashboard-container">
+    <component :is="currentRole" />
+  </div>
+</template>
+
+<script>
+  import {
+    mapGetters
+  } from 'vuex'
+
+  export default {
+    name: 'Dashboard',
+    components: {},
+    data() {
+      return {
+        currentRole: 'adminDashboard'
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'roles'
+      ])
+    },
+    created() {
+      if (!this.roles.includes('admin')) {
+        this.currentRole = 'editorDashboard'
+      }
+    }
+  }
+
+</script>
