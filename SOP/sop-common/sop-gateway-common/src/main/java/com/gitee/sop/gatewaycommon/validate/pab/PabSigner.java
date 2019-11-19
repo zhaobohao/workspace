fc02fe93f7ca4efe18f9d173c666423cd384e0af
zhaobohao <zhaobohao@sohu.com>
2019-11-19 17:43:35 +0800

    增加新的pab加密签名方式

--- SOP/sop-common/sop-gateway-common/src/main/java/com/gitee/sop/gatewaycommon/validate/pab/PabSigner.java
+++ SOP/sop-common/sop-gateway-common/src/main/java/com/gitee/sop/gatewaycommon/validate/pab/PabSigner.java
@@ -6,15 +6,18 @@ import com.gitee.sop.gatewaycommon.validate.Signer;
 
 /**
  * 平安外联组签名验证实现。
+ *  做为银行方或者渠道方，手里会有对方的公钥和自己的私钥。
+ *  在做data加密的时候用公钥。加签的时候用自己的私钥。
  *
+ *  同理，当对方的报文过来的时候，data解密用自己的私钥。
+ *  在验签的时候，用对方的公钥。
  * @author zhaobo
  */
 public class PabSigner implements Signer {
 
     @Override
-    public boolean checkSign(ApiParam apiParam, String secret) {
+    public boolean checkSign(ApiParam apiParam, String publicKeyISV) {
         // 服务端存的是公钥
-        String publicKeyISV = secret;
         String charset = apiParam.fetchCharset();
         String signType = apiParam.fetchSignMethod();
         if (signType == null) {
