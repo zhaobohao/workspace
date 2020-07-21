aba0ee82f7f23728d04d7fe159e1b3a402ea5764
zhaobohao <zhaobohao@sohu.com>
2020-07-21 16:11:03 +0800

    fixed hot items count.

--- springBootExample/springboot-flink/src/main/java/com/flink/example/datastreamapi/HotItems.java
+++ springBootExample/springboot-flink/src/main/java/com/flink/example/datastreamapi/HotItems.java
@@ -2,15 +2,19 @@ package com.flink.example.datastreamapi;
 
 import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
 import org.apache.flink.api.java.io.PojoCsvInputFormat;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple1;
 import org.apache.flink.api.java.typeutils.PojoTypeInfo;
 import org.apache.flink.api.java.typeutils.TypeExtractor;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
 import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
@@ -18,6 +22,10 @@ import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 import java.io.File;
 import java.net.URL;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 
 public class HotItems {
@@ -57,7 +65,14 @@ public class HotItems {
         DataStream<ItemViewCount> windowedData = pvData.keyBy("itemId")
                 .timeWindow(Time.seconds(10), Time.seconds(5)).aggregate(new CountAgg(), new WindowResultFunction());
         windowedData.print().setParallelism(1);
-        env.execute("streaming word count");
+
+        DataStream<String> topItems = windowedData
+                .keyBy("windowEnd")
+                .process(new TopNHotItems(3));  // 求点击量前3名的商品
+
+        topItems.print();
+        env.execute("Hot Items Job");
+
     }
 
     private static class CountAgg implements AggregateFunction<UserBehavior, Long, Long> {
@@ -85,10 +100,15 @@ public class HotItems {
     private static class WindowResultFunction implements WindowFunction<Long, ItemViewCount, Tuple, TimeWindow> {
 
         @Override
-        public void apply(Tuple tuple, TimeWindow timeWindow, Iterable<Long> iterable, Collector<ItemViewCount> collector) throws Exception {
-            Long itemid = ((Tuple1<Long>) tuple).f0;
-            Long count = iterable.iterator().next();
-            collector.collect(ItemViewCount.of(itemid, timeWindow.getEnd(), count));
+        public void apply(
+                Tuple key,  // 窗口的主键，即 itemId
+                TimeWindow window,  // 窗口
+                Iterable<Long> aggregateResult, // 聚合函数的结果，即 count 值
+                Collector<ItemViewCount> collector  // 输出类型为 ItemViewCount
+        ) throws Exception {
+            Long itemId = ((Tuple1<Long>) key).f0;
+            Long count = aggregateResult.iterator().next();
+            collector.collect(ItemViewCount.of(itemId, window.getEnd(), count));
         }
 
     }
@@ -96,3 +116,71 @@ public class HotItems {
 
 }
 
+/** 求某个窗口中前 N 名的热门点击商品，key 为窗口时间戳，输出为 TopN 的结果字符串 */
+class TopNHotItems extends KeyedProcessFunction<Tuple, ItemViewCount, String> {
+
+    private final int topSize;
+
+    public TopNHotItems(int topSize) {
+        this.topSize = topSize;
+    }
+
+    // 用于存储商品与点击数的状态，待收齐同一个窗口的数据后，再触发 TopN 计算
+    private ListState<ItemViewCount> itemState;
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+        // 状态的注册
+        ListStateDescriptor<ItemViewCount> itemsStateDesc = new ListStateDescriptor<>(
+                "itemState-state",
+                ItemViewCount.class);
+        itemState = getRuntimeContext().getListState(itemsStateDesc);
+    }
+
+    @Override
+    public void processElement(
+            ItemViewCount input,
+            Context context,
+            Collector<String> collector) throws Exception {
+
+        // 每条数据都保存到状态中
+        itemState.add(input);
+        // 注册 windowEnd+1 的 EventTime Timer, 当触发时，说明收齐了属于windowEnd窗口的所有商品数据
+        context.timerService().registerEventTimeTimer(input.windowEnd + 1);
+    }
+
+    @Override
+    public void onTimer(
+            long timestamp, OnTimerContext ctx, Collector<String> out) throws Exception {
+        // 获取收到的所有商品点击量
+        List<ItemViewCount> allItems = new ArrayList<>();
+        for (ItemViewCount item : itemState.get()) {
+            allItems.add(item);
+        }
+        // 提前清除状态中的数据，释放空间
+        itemState.clear();
+        // 按照点击量从大到小排序
+        allItems.sort(new Comparator<ItemViewCount>() {
+            @Override
+            public int compare(ItemViewCount o1, ItemViewCount o2) {
+                return (int) (o2.viewCount - o1.viewCount);
+            }
+        });
+        // 将排名信息格式化成 String, 便于打印
+        StringBuilder result = new StringBuilder();
+        result.append("====================================\n");
+        result.append("时间: ").append(new Timestamp(timestamp-1)).append("\n");
+        for (int i=0;i<topSize;i++) {
+            ItemViewCount currentItem = allItems.get(i);
+            // No1:  商品ID=12224  浏览量=2413
+            result.append("No").append(i).append(":")
+                    .append("  商品ID=").append(currentItem.itemId)
+                    .append("  浏览量=").append(currentItem.viewCount)
+                    .append("\n");
+        }
+        result.append("====================================\n\n");
+
+        out.collect(result.toString());
+    }
+}
